44f9cc57383c78ec8ce205b696bcea841f9c28f4
Christian Knabenhans <christian.knabenhans@epfl.ch>
2024-04-02 19:40:10 +0200

    Rename Crs to PublicParameters for relations

--- src/relations/mod.rs
+++ src/relations/mod.rs
@@ -1,7 +1,3 @@
 pub mod labrador;
-pub mod ccs;
-pub mod cm_ccs;
 pub mod ajtai_cm;
 pub mod traits;
-pub mod eval_ccs;
-pub mod eval;
--- src/relations/traits.rs
+++ src/relations/traits.rs
@@ -1,7 +1,9 @@
 pub trait Relation {
+    type PublicParameters;
     type Instance;
     type Witness;
-    type Crs;
 
-    fn is_satisfied(crs: &Self::Crs, x: &Self::Instance, w: &Self::Witness) -> bool;
+    fn is_well_defined(pp: &Self::PublicParameters, x: &Self::Instance, w: Option<&Self::Witness>) -> bool;
+
+    fn is_satisfied(pp: &Self::PublicParameters, x: &Self::Instance, w: &Self::Witness) -> bool;
 }
