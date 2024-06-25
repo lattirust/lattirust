cfebcffa1f2e152b3e8b36310bd2ada25f6d7fc6
Isaac <isaacvil@gmail.com>
2024-06-24 22:40:59 -0600

    relative prime powers for decomposition

--- lattirust-arithmetic/src/partial_ntt.rs
+++ lattirust-arithmetic/src/partial_ntt.rs
@@ -1,6 +1,6 @@
 // Partial NTT
 
-use crate::ring::Zq;
+use crate::ring::{Ring, Zq};
 
 const fn primitive_root_of_unity<const Q: u64, const Z: usize>() -> u64 {
     // Find the root of unity such that w^z = 1
@@ -30,9 +30,38 @@ pub trait PartialNTT<
         root_of_unity_relative_powers::<Q, Z, PHI_Z>(Self::ROOT_OF_UNITY);
 
     fn ntt(a: &mut [Zq<Q>; N]) {
+        let rou = Zq::<Q>::from(Self::ROOT_OF_UNITY);
         // 1. Split array in chunks of size D
+        let components = coprimes_set::<Z, PHI_Z>().map(|power| {
+            let rj = rou.pow([power as u64]);
+        });
         // 2. reduce polynomial for each chunk with the corresponding relative power of the root of
         //    unity
         todo!()
     }
+}
+
+const fn coprimes_set<const Z: usize, const PHI_Z: usize>() -> [usize; PHI_Z] {
+    let mut i = 0;
+    let mut j = 0;
+    let mut set = [0; PHI_Z];
+    while i < Z {
+        if gcd::<Z>(i) {
+            set[j] = i;
+            j += 1;
+        }
+        i += 1;
+    }
+    set
+}
+
+const fn gcd<const Z: usize>(i: usize) -> bool {
+    let mut a = i;
+    let mut b = Z;
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp
+    }
+    a == 1
 }
