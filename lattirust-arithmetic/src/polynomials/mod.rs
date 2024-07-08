c860a9f0aabe1ed5cb99909d5bee00348ace3e5f
Alex Klein <matthewklein345@gmail.com>
2024-07-08 14:02:58 +0200

    Add exports for multilinear polynomial

--- lattirust-arithmetic/src/polynomials/mod.rs
+++ lattirust-arithmetic/src/polynomials/mod.rs
@@ -6,4 +6,19 @@ mod virtual_polynomial;
 
 //TODO add exports
 pub use errors::ArithErrors;
+pub use multilinear_polynomial::{
+    evaluate_no_par,
+    evaluate_opt,
+    fix_last_variables,
+    fix_last_variables_no_par,
+    fix_variables,
+    identity_permutation,
+    identity_permutation_mles,
+    merge_polynomials,
+    random_mle_list,
+    random_permutation,
+    random_permutation_mles,
+    random_zero_mle_list,
+    DenseMultilinearExtension,
+};
 pub use util::{ bit_decompose, gen_eval_point, get_batched_nv, get_index };
