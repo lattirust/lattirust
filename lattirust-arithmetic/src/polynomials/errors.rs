a2215964ac622cf48e1f365576874d775618cd8d
Alex Klein <matthewklein345@gmail.com>
2024-07-08 13:40:19 +0200

    Add errors

---
+++ lattirust-arithmetic/src/polynomials/errors.rs
@@ -0,0 +1,26 @@
+// Copyright (c) 2023 Espresso Systems (espressosys.com)
+// This file is part of the HyperPlonk library.
+
+// Adapted for rings by Nethermind
+
+//! Error module.
+
+use ark_std::string::String;
+use displaydoc::Display;
+
+/// A `enum` specifying the possible failure modes of the arithmetics.
+#[derive(Display, Debug)]
+pub enum ArithErrors {
+    /// Invalid parameters: {0}
+    InvalidParameters(String),
+    /// Should not arrive to this point
+    ShouldNotArrive,
+    /// An error during (de)serialization: {0}
+    SerializationErrors(ark_serialize::SerializationError),
+}
+
+impl From<ark_serialize::SerializationError> for ArithErrors {
+    fn from(e: ark_serialize::SerializationError) -> Self {
+        Self::SerializationErrors(e)
+    }
+}
