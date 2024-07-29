313063c17a9c00c8206d05932de1bf387946836c
Alex Klein <matthewklein345@gmail.com>
2024-07-29 19:52:01 +0200

    Move overfield to associated type in latticefold challenge set

--- lattirust-arithmetic/src/challenge_set/latticefold_challenge_set.rs
+++ lattirust-arithmetic/src/challenge_set/latticefold_challenge_set.rs
@@ -1,24 +1,27 @@
 use ark_ff::Field;
 
-use crate::ring::{PolyRing, Pow2CyclotomicPolyRingNTT, Zq};
+use crate::ring::{ PolyRing, Pow2CyclotomicPolyRingNTT, Zq };
 
-pub trait OverField<F: Field>: PolyRing {
-    fn field_to_base_ring(f: &F) -> Self::BaseRing;
+pub trait OverField: PolyRing {
+    type F: Field;
+
+    fn field_to_base_ring(f: &Self::F) -> Self::BaseRing;
 }
 
-impl<const Q: u64, const N: usize> OverField<Zq<Q>> for Pow2CyclotomicPolyRingNTT<Q, N> {
-    fn field_to_base_ring(f: &Zq<Q>) -> Self::BaseRing {
+impl<const Q: u64, const N: usize> OverField for Pow2CyclotomicPolyRingNTT<Q, N> {
+    type F = Zq<Q>;
+
+    fn field_to_base_ring(f: &Self::F) -> Self::BaseRing {
         *f
     }
 }
-
-pub trait LatticefoldChallengeSet<F: Field, R: OverField<F> + PolyRing> {
+pub trait LatticefoldChallengeSet<R: OverField + PolyRing> {
     fn small_challenge_coefficient_from_random_bytes(i: usize, bs: &[u8]) -> R::BaseRing;
     fn small_challenge_from_random_bytes(d: usize, bs: &[u8]) -> R {
         <R as From<Vec<R::BaseRing>>>::from(
             (0..d)
                 .map(|i| Self::small_challenge_coefficient_from_random_bytes(i, &bs[i..]))
-                .collect(),
+                .collect()
         )
     }
 
@@ -28,22 +31,17 @@ pub trait LatticefoldChallengeSet<F: Field, R: OverField<F> + PolyRing> {
     }
 
     #[inline]
-    fn big_challenge_from_field(f: &F) -> R::BaseRing {
+    fn big_challenge_from_field(f: &R::F) -> R::BaseRing {
         R::field_to_base_ring(f)
     }
 }
 
 pub struct BinarySmallSet<const Q: u64, const N: usize>;
 
-impl<const Q: u64, const N: usize> LatticefoldChallengeSet<Zq<Q>, Pow2CyclotomicPolyRingNTT<Q, N>>
-    for BinarySmallSet<Q, N>
-{
+impl<const Q: u64, const N: usize> LatticefoldChallengeSet<Pow2CyclotomicPolyRingNTT<Q, N>>
+for BinarySmallSet<Q, N> {
     fn small_challenge_coefficient_from_random_bytes(_i: usize, bs: &[u8]) -> Zq<Q> {
-        if bs[0] == 0 {
-            <Zq<Q> as Field>::ZERO
-        } else {
-            <Zq<Q> as Field>::ONE
-        }
+        if bs[0] == 0 { <Zq<Q> as Field>::ZERO } else { <Zq<Q> as Field>::ONE }
     }
 
     fn small_challenge(bs: &[u8]) -> Pow2CyclotomicPolyRingNTT<Q, N> {
