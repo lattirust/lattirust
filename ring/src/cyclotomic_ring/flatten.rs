042fc29a658e9222b61ed196e27c845cb537c1eb
Ilia Vlasov <elijah.vlasov@gmail.com>
2024-11-04 17:47:09 +0000

    Add promote_from_coeffs

--- ring/src/cyclotomic_ring/flatten.rs
+++ ring/src/cyclotomic_ring/flatten.rs
@@ -10,6 +10,11 @@ pub trait Flatten<C: CyclotomicConfig<N>, const N: usize> {
     fn flatten_to_coeffs(self) -> Vec<C::BaseCRTField>;
 }
 
+/// A trait to implement `promote_from_coeffs` on the foreign type `Vec`.
+pub trait Promote<C: CyclotomicConfig<N>, const N: usize, const D: usize> {
+    fn promote_from_coeffs(self) -> Option<Vec<CyclotomicPolyRingNTTGeneral<C, N, D>>>;
+}
+
 impl<C: CyclotomicConfig<N>, const N: usize, const D: usize> Flatten<C, N>
     for Vec<CyclotomicPolyRingNTTGeneral<C, N, D>>
 {
@@ -17,6 +22,30 @@ impl<C: CyclotomicConfig<N>, const N: usize, const D: usize> Flatten<C, N>
         let (ptr, len, cap) = self.into_raw_parts();
 
         unsafe { Vec::from_raw_parts(ptr as *mut C::BaseCRTField, len * D, cap * D) }
+    }
+}
+
+impl<C: CyclotomicConfig<N>, const N: usize, const D: usize> Promote<C, N, D>
+    for Vec<C::BaseCRTField>
+{
+    fn promote_from_coeffs(mut self) -> Option<Vec<CyclotomicPolyRingNTTGeneral<C, N, D>>> {
+        if self.len() % D != 0 {
+            return None;
+        }
+
+        if self.capacity() % D != 0 {
+            self.shrink_to_fit();
+        }
+
+        let (ptr, len, cap) = self.into_raw_parts();
+
+        Some(unsafe {
+            Vec::from_raw_parts(
+                ptr as *mut CyclotomicPolyRingNTTGeneral<C, N, D>,
+                len / D,
+                cap / D,
+            )
+        })
     }
 }
 
@@ -62,6 +91,42 @@ mod tests {
                 Fq3::from(42u32),
                 Fq3::from(42u32),
             ]
+        )
+    }
+
+    #[test]
+    fn test_promote_ntt() {
+        let vec: Vec<Fq3> = vec![
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::one(),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(3u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+            Fq3::from(42u32),
+        ];
+        let promoted = vec.promote_from_coeffs().unwrap();
+
+        assert_eq!(
+            promoted,
+            vec![RqNTT::one(), RqNTT::from(3u32), RqNTT::from(42u32)]
         )
     }
 }
--- ring/src/cyclotomic_ring/mod.rs
+++ ring/src/cyclotomic_ring/mod.rs
@@ -1,11 +1,11 @@
 pub mod models;
 
 mod coeff_form;
-mod flatten;
 mod ntt_form;
 mod ring_config;
 
+pub mod flatten;
+
 pub use coeff_form::*;
-pub use flatten::*;
 pub use ntt_form::*;
 pub use ring_config::*;
