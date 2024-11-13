26652e3d489a7eddbbc7065bdb30e3b2a9719b17
Jacomo <jacomo@Jacomos-MacBook-Pro.local>
2024-11-13 11:47:35 +0100

    cleaning

--- commitment/openfhe/openfhe-dgs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "rust/cxx.h"
-#include "openfhe/core/math/discretegaussiangeneratorgeneric.h"
-#include "openfhe/core/math/discretegaussiangenerator-impl.h"
-#include "openfhe/core/math/discretegaussiangenerator.h"
-
-struct GaussianParameters;
-
-// std::unique_ptr<DiscreteGaussianGenerator> new_gaussian_sampler(const GaussianParameters& params);
